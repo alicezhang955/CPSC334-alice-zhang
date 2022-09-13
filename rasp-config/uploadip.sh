72ac12956444beb1b980f498326bacffb0b88b03
Alice Zhang <alicezhang955@gmail.com>
2022-09-12 20:29:24 -0400

    upload current IP

--- rasp-config/uploadip.sh
+++ rasp-config/uploadip.sh
@@ -5,7 +5,7 @@ cd /home/student334/CPSC334-alice-zhang
 sleep 20
 
 git pull
-/sbin/ifconfig -a > /home/student334/CPSC334-alice-zhang/rasp-config/ip.md
+hostname -I > /home/student334/CPSC334-alice-zhang/rasp-config/ip.md
 
 git add -A
 git commit -m "upload current IP"
