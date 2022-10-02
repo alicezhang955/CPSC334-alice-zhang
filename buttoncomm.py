e51eb6b3fe5b4daba5ba09d0abe0a4ec4d5bfc83
Alice Zhang <alicezhang955@gmail.com>
2022-10-02 14:23:50 -0400

    edit buttoncomm

--- buttoncomm.py
+++ buttoncomm.py
@@ -1,7 +1,13 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import serial
 import time
 
 butpin = 26
+SETUP = False
+MAX_BUFF_LEN = 255
+port = None
+
+prev = time.time()
 
 while(not SETUP):
     try:
@@ -15,22 +21,27 @@ while(not SETUP):
 
     if(port is not None): # We're connected
         SETUP = True
+        print("connected")
 
 
 def button_callback(channel):
     print("Reset board!")
 
+def read_ser(num_char = 1):
+    string = port.read(num_char)
+    return string.decode()
 
 def main():
-
-    GPIO.setup(butpin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    global butpin
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(butpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(butpin,GPIO.RISING,callback=button_callback, bouncetime = 500) 
 
-    # while(1):
-    #     string = read_ser(MAX_BUFF_LEN)
-    #     if(len(string)):
-    #         print(string)
+    while(1):
+        count = 1
+        print(GPIO.input(butpin))
 
+        time.sleep(1)
     #     cmd = input() # Blocking, there're solutions for this ;)
     #     if(cmd):
     #         write_ser(cmd)
