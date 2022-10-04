6d8baba172403695ca9c7e17052628e683e5fe2e
alicezhang955 <alicezhang955@gmail.com>
2022-10-04 19:44:20 -0400

    reorganize files

--- singlem2t2.py
+++ singlem2t2.py
@@ -25,7 +25,7 @@ gameOver = 0
 while(not SETUP):
     try:
     # 					 Serial port(windows-->COM), baud rate, timeout msg
-        port = serial.Serial("/dev/ttyUSB1", 115200, timeout=1)
+        port = serial.Serial("/dev/ttyUSB0", 115200, timeout=1)
 
     except: # Bad way of writing excepts (always know your errors)
         if(time.time() - prev > 2): # Don't spam with msg
